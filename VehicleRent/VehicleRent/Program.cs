d90b406dc7ad666c531609ccfad7c62e765391ba
mugiwaranosri <sriramskullzz1999@gmail.com>
2024-07-16 13:56:29 -0400

    Final

--- VehicleRent/VehicleRent/Program.cs
+++ VehicleRent/VehicleRent/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RentalAgency agency = new RentalAgency();
+
+        Car car1 = new Car("Model S", "Porsche", 2022, 150m, 5, "Electric", "Automatic", true);
+        Truck truck1 = new Truck("Mustang", "Ford", 2021, 100m, 3.5, "Pickup", true);
+        Motorcycle bike1 = new Motorcycle("Ninja", "Kawasaki", 2020, 80m, 650, "Gasoline", true);
+
+        agency.AddVehicle(car1);
+        agency.AddVehicle(truck1);
+        agency.AddVehicle(bike1);
+
+        agency.DisplayFleet();
+
+        agency.RentVehicle(car1, 5);
+
+        agency.DisplayFleet();
+    }
+}
--- VehicleRent/VehicleRent/obj/Debug/net8.0/VehicleRent.AssemblyInfo.cs
+++ VehicleRent/VehicleRent/obj/Debug/net8.0/VehicleRent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleRent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3530574c7975359af75a000271b9729faaa99a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9aa7becfbb7e791b0943ca272c4677377c6e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleRent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleRent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
